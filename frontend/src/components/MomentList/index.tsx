e3fce5c745c007f123527b1d6812fb356d38fd5a
Michael Wang <michaelwan@zillowgroup.com>
2021-03-17 19:32:06 -0700

    Add basic table display

--- frontend/src/components/MomentList/index.tsx
+++ frontend/src/components/MomentList/index.tsx
@@ -1,3 +1,43 @@
-export default function MomentList() {
-  return <div></div>;
+import React from 'react';
+import { Table } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+
+import { CardInfo } from '../../App';
+
+type MomentListProps = {
+  cardsData: CardInfo[],
+  removeCard: Function
+}
+
+const columns: ColumnsType<CardInfo> = [
+  {
+    key: 'cardName',
+    title: 'Card Name',
+    dataIndex: 'cardName',
+  },
+  {
+    key: 'setName',
+    title: 'Set Name',
+    dataIndex: 'setName',
+  },
+  {
+    key: 'lowestPrice',
+    title: 'Lowest Price',
+    dataIndex: 'lowestPrice',
+  },
+  {
+    key: 'diffNextLowest',
+    title: 'Difference between next lowest price',
+    dataIndex: 'diffNextLowest',
+  }
+];
+
+export default function MomentList({cardsData, removeCard}: MomentListProps) {
+  return (
+    <Table<CardInfo>
+      pagination={false}
+      columns={columns}
+      dataSource={cardsData}
+    />
+  );
 }
