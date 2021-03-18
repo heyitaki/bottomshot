6fa0467291f15e6d7b258bfd2caee3ec600c0d91
heyitaki <akshath@berkeley.edu>
2021-03-17 19:24:45 -0700

    adjust get method to take in arbitrary ids

--- server/src/test.py
+++ server/src/test.py
@@ -2,38 +2,37 @@ import pprint
 
 import requests
 
-seeing_stars_set_id = "122b048d-585e-4c63-8275-c23949576fd6"
-player_ids = [
-    "d54193d1-e0ed-4217-b2d1-884406d78acf",  # embiid
-    "dfff6981-37e2-43c3-ada5-ab6e3b007b99",  # tatum
-    "665aaac2-f124-4303-a945-e002a4a3f298",  # booker
-]
 
-
-def construct_payload(set_id, player_id):
+def construct_payload(set_id, play_id):
     return {
         "operationName": "GetUserMomentListingsDedicated",
-        "variables": {"input": {"setID": set_id, "playID": player_id}},
+        "variables": {"input": {"setID": set_id, "playID": play_id}},
         "query": "query GetUserMomentListingsDedicated($input: GetUserMomentListingsInput!) {\n  getUserMomentListings(input: $input) {\n    data {\n      circulationCount\n      flowRetired\n      version\n      set {\n        id\n        flowName\n        flowSeriesNumber\n        __typename\n      }\n      play {\n        ... on Play {\n          ...PlayDetails\n          __typename\n        }\n        __typename\n      }\n      assetPathPrefix\n      priceRange {\n        min\n        max\n        __typename\n      }\n      momentListings {\n        id\n        moment {\n          id\n          price\n          flowSerialNumber\n          owner {\n            dapperID\n            username\n            profileImageUrl\n            __typename\n          }\n          setPlay {\n            ID\n            flowRetired\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      momentListingCount\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment PlayDetails on Play {\n  id\n  description\n  stats {\n    playerID\n    playerName\n    primaryPosition\n    currentTeamId\n    dateOfMoment\n    jerseyNumber\n    awayTeamName\n    awayTeamScore\n    teamAtMoment\n    homeTeamName\n    homeTeamScore\n    totalYearsExperience\n    teamAtMomentNbaId\n    height\n    weight\n    currentTeam\n    birthplace\n    birthdate\n    awayTeamNbaId\n    draftYear\n    nbaSeason\n    draftRound\n    draftSelection\n    homeTeamNbaId\n    draftTeam\n    draftTeamNbaId\n    playCategory\n    homeTeamScoresByQuarter {\n      quarterScores {\n        type\n        number\n        sequence\n        points\n        __typename\n      }\n      __typename\n    }\n    awayTeamScoresByQuarter {\n      quarterScores {\n        type\n        number\n        sequence\n        points\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  statsPlayerGameScores {\n    blocks\n    points\n    steals\n    assists\n    minutes\n    rebounds\n    turnovers\n    plusMinus\n    flagrantFouls\n    personalFouls\n    playerPosition\n    technicalFouls\n    twoPointsMade\n    blockedAttempts\n    fieldGoalsMade\n    freeThrowsMade\n    threePointsMade\n    defensiveRebounds\n    offensiveRebounds\n    pointsOffTurnovers\n    twoPointsAttempted\n    assistTurnoverRatio\n    fieldGoalsAttempted\n    freeThrowsAttempted\n    twoPointsPercentage\n    fieldGoalsPercentage\n    freeThrowsPercentage\n    threePointsAttempted\n    threePointsPercentage\n    __typename\n  }\n  statsPlayerSeasonAverageScores {\n    minutes\n    blocks\n    points\n    steals\n    assists\n    rebounds\n    turnovers\n    plusMinus\n    flagrantFouls\n    personalFouls\n    technicalFouls\n    twoPointsMade\n    blockedAttempts\n    fieldGoalsMade\n    freeThrowsMade\n    threePointsMade\n    defensiveRebounds\n    offensiveRebounds\n    pointsOffTurnovers\n    twoPointsAttempted\n    assistTurnoverRatio\n    fieldGoalsAttempted\n    freeThrowsAttempted\n    twoPointsPercentage\n    fieldGoalsPercentage\n    freeThrowsPercentage\n    threePointsAttempted\n    threePointsPercentage\n    __typename\n  }\n  __typename\n}\n",
     }
 
 
-def get_min_prices():
-    data = []
-    for player_id in player_ids:
-        payload = construct_payload(seeing_stars_set_id, player_id)
-        headers = {"content-type": "application/json"}
-        r = requests.post(
-            "https://api.nbatopshot.com/marketplace/graphql?GetUserMomentListingsDedicated",
-            json=payload,
-            headers=headers,
-        )
-        res = r.json()
-        player_name = res["data"]["getUserMomentListings"]["data"]["play"]["stats"][
-            "playerName"
-        ]
-        min_price = res["data"]["getUserMomentListings"]["data"]["priceRange"]["min"]
-        line_item = player_name + " - " + min_price
-        pprint.pprint(line_item)
-        data.append(line_item)
-    return data
+def get_moment_data(set_id, play_id):
+    payload = construct_payload(set_id, play_id)
+    headers = {"content-type": "application/json"}
+    r = requests.post(
+        "https://api.nbatopshot.com/marketplace/graphql?GetUserMomentListingsDedicated",
+        json=payload,
+        headers=headers,
+    )
+    topshot_data = r.json()["data"]["getUserMomentListings"]["data"]
+    moment_data = {
+        "name": topshot_data["play"]["stats"]["playerName"],
+        "min_price": topshot_data["priceRange"]["min"],
+    }
+    pprint.pprint(moment_data)
+    return moment_data
+
+
+seeing_stars_set_id = "122b048d-585e-4c63-8275-c23949576fd6"
+play_ids = [
+    "d54193d1-e0ed-4217-b2d1-884406d78acf",  # embiid
+    "dfff6981-37e2-43c3-ada5-ab6e3b007b99",  # tatum
+    "665aaac2-f124-4303-a945-e002a4a3f298",  # booker
+]
+for play_id in play_ids:
+    get_moment_data(seeing_stars_set_id, play_id)
